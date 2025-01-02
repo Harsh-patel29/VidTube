5dd506a2538525360483114f7256023a34c8b715
Harsh-patel29 <harshbharat55@gmail.com>
2025-01-02 11:19:58 +0530

    Models create

---
+++ src/models/user.models.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudnary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudnary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Video',
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, 'Password Required'],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
